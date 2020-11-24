967ab9b8b177351f1cad5536000cb166c1ef8134
AleksandrGolubnik <53440831+AleksandrGolubnik@users.noreply.github.com>
2020-11-25 02:09:16 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,9 +1,16 @@
 <?php
 $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
 $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+$url=parse_url(getenv("mysql://bdf7379cdd2da2:3225ad60@eu-cdbr-west-03.cleardb.net/heroku_65b88f02bf3374f?reconnect=true"));
 
+    $server = $url["eu-cdbr-west-03.cleardb.net"];
+    $username = $url["bdf7379cdd2da2"];
+    $password = $url["3225ad60"];
+    $db = substr($url["heroku_65b88f02bf3374f"],1);
+   mysqli_connect($server, $username, $password);
+
+    mysqli_select_db($db);
 $pass = md5($pass."fsdfds");
-$mysql = new mysqli('localhost', 'root', '94Baksizu-', 'register-bd');
 $result = $mysql -> query("SELECT * FROM `users` WHERE `login` = '$login' AND  `pass` = '$pass'");
 $user = $result->fetch_assoc();
 if(count($user) == 0) {
