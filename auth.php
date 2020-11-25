8395ae236d71a045c89f6f08d7cfbdb062fcb830
AleksandrGolubnik <53440831+AleksandrGolubnik@users.noreply.github.com>
2020-11-25 13:36:31 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -6,10 +6,10 @@ $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
 $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
 $url=parse_url(getenv("mysql://bdf7379cdd2da2:3225ad60@eu-cdbr-west-03.cleardb.net/heroku_65b88f02bf3374f?reconnect=true"));
 
-    $server = $url["eu-cdbr-west-03.cleardb.net"];
-    $username = $url["bdf7379cdd2da2"];
-    $password = $url["3225ad60"];
-    $db = substr($url["heroku_65b88f02bf3374f"],1);
+    $server = $url['eu-cdbr-west-03.cleardb.net'];
+    $username = $url['bdf7379cdd2da2'];
+    $password = $url['3225ad60'];
+    $db = substr($url['heroku_65b88f02bf3374f'],1);
    $mysql= mysqli_connect($server, $username, $password, $db);
 
 
