b265309fc837fa2c720ec931930c733e95a66803
AleksandrGolubnik <53440831+AleksandrGolubnik@users.noreply.github.com>
2020-11-25 03:02:39 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -7,9 +7,9 @@ $url=parse_url(getenv("mysql://bdf7379cdd2da2:3225ad60@eu-cdbr-west-03.cleardb.n
     $username = $url["bdf7379cdd2da2"];
     $password = $url["3225ad60"];
     $db = substr($url["heroku_65b88f02bf3374f"],1);
-   mysqli_connect($server, $username, $password);
+   $mysql= mysqli_connect($server, $username, $password, $db);
 
-    mysqli_select_db($db);
+
 $pass = md5($pass."fsdfds");
 $result = $mysql -> query("SELECT * FROM `users` WHERE `login` = '$login' AND  `pass` = '$pass'");
 $user = $result->fetch_assoc();
