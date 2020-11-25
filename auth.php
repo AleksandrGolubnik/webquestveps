e4b7ab2ac158f252d5d0a446d0dadc263889b3c1
AleksandrGolubnik <53440831+AleksandrGolubnik@users.noreply.github.com>
2020-11-25 13:37:34 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -10,7 +10,7 @@ $url=parse_url(getenv("mysql://bdf7379cdd2da2:3225ad60@eu-cdbr-west-03.cleardb.n
     $username = $url['bdf7379cdd2da2'];
     $password = $url['3225ad60'];
     $db = substr($url['heroku_65b88f02bf3374f'],1);
-   $mysql= mysqli_connect($server, $username, $password, $db);
+   $mysql= new mysqli($server, $username, $password, $db);
 
 
 $pass = md5($pass."fsdfds");
